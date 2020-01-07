b6c420285e4e818989948283073b610a03e3ca72
kevinkim1030 <kevin.kim1030@gmail.com>
2020-01-07 14:17:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Your code here
+
+let dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+function moveDodgerRight(){
+  let leftNumbers = dodger.style.left.replace('px', '')
+  let left = parseInt(leftNumbers, 10)
+
+  if (left > 0){
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+
+// describe('moveDodgerRight', () => {
+//   beforeEach(() => {
+//     dodger = document.getElementById('dodger')
+//   })
+
+//   it('moves the DODGER to the right', () => {
+//     var left = dodger.style.left
+//     left = parseInt(left)
+
+//     moveDodgerRight()
+
+//     var newPosition = dodger.style.left
+//     newPosition = parseInt(newPosition)
+
+//     expect(newPosition).to.be.above(left)
+//   })
+// })
