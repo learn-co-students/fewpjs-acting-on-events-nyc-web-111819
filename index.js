1293620c0426e9fbbf3e1369a37f326b709d3f12
jasmosez <jas.schaffer@gmail.com>
2020-01-08 17:35:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Your code here
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left < (400 - dodger.style.width)) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+
+
+function keydownHandler() {
+  switch (event.key) {
+  case "ArrowLeft":
+    moveDodgerLeft();
+    break;
+  
+    case "ArrowRight": 
+    moveDodgerRight();
+    break;
+    
+  case "ArrowUp":
+    // moveDodgerUp();
+    console.log('Move Up!')
+    break;
+  
+  case "ArrowDown":
+    // moveDodgerDown();
+    console.log('Move Down!')
+    break;
+    
+  }
+  
+};
+
+let dodger = document.getElementById("dodger");
+
+document.addEventListener("keydown", keydownHandler)
